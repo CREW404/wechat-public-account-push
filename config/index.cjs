0bcf5d13fd22fd92ba9e68aa4fc9edf19de94772
CREW404 <145235642+CREW404@users.noreply.github.com>
2023-09-17 01:01:52 +0100

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,6 +6,8 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
+  // 请将以下代码按照图示粘贴到对应位置
+  USE_PASSAGE: 'push-deer',
 
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx148ba10959e9a0bb',
@@ -21,9 +23,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小亭',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU25496TyvW4aGdaSpWM3GINmKdmHibkeKQoY1Ls',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ohxb_6sV7PjS4pKU9kD-gnUX536c',
+      useTemplateId: '8Ge7ew6S-XhvaffxYQ3uD065hcLKLollobNP3oS2F8M',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-20',
       festivals: [
